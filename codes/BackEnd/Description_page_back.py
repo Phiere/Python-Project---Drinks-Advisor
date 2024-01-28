5cd402fd9f7141f63c7b33b3ef98df58c0d6357f
Phiere <123893816+Phiere@users.noreply.github.com>
2024-01-28 18:33:53 +0100

    Nettoyage Description + debug 2

---
+++ codes/BackEnd/Description_page_back.py
@@ -0,0 +1,122 @@
+import Db_gestions as Db
+import pandas as pd
+
+def get_name_from_drink():
+    db,index = Db.choix_de_la_data_base,Db.index_boisson
+    boisson = Db.dbsall[db][0].iloc[index]
+    name = boisson['Name']
+    return name 
+
+def get_description_from_drink():
+    db,index = Db.choix_de_la_data_base,Db.index_boisson
+    boisson = Db.dbsall[db][0].iloc[index]
+    colonnes = Db.dbsall[db][0].columns
+    text = ""
+    for i in range(len(boisson)) :
+        colonne = colonnes[i]
+        if colonne not in ['Unnamed : 0','PersonalRating','Name','Favories']:
+            text += str(colonne) + ' : ' + str(boisson[i]) + '\n' + '\n'
+    return text
+
+def get_status_favori():
+    db,index =  Db.choix_de_la_data_base,Db.index_boisson
+    favory = Db.dbsall[db][0].iloc[index][-1]
+    return favory
+
+def update_status_favori():
+    db,index =  Db.choix_de_la_data_base, Db.index_boisson
+    favory = not(Db.dbsall[db][0].iloc[index][-1])
+    Db.dbsall[db][0].iloc[index,-1] = favory
+    return favory
+
+def get_comment():
+    db,index =  Db.choix_de_la_data_base, Db.index_boisson
+    comment = Db.dbsall[db][0].iloc[index][-2]
+    if pd.isna(comment): return 'Laisser un commentaire sur votre boisson'
+    return comment
+
+def update_comment(commentary):
+    db,index =  Db.choix_de_la_data_base, Db.index_boisson
+    Db.dbsall[db][0].iloc[index,-2] = commentary
+
+
+def get_rating():
+    db,index = Db.choix_de_la_data_base,  Db.index_boisson
+    rating = Db.dbsall[db][0].iloc[index][-3]
+    return rating
+
+def update_rating(rating):
+    db,index =  Db.choix_de_la_data_base,Db.index_boisson
+    Db.dbsall[db][0].iloc[index,-3] = rating
+
+
+    
+'''if True : 
+        # Wines
+        if Db.choix_de_la_data_base == 0:
+            real_names = ['country', 'description', 'price', 'province', 'variety', 'winery', 'region_']
+            column_names = ['Provenance', 'Description', 'Prix', 'Province', 'Variété', 'Domaine', 'Région']
+        # Cocktails
+        elif Db.choix_de_la_data_base == 1: 
+            real_names = ['strAlcoholic', 'strCategory', 'strDrinkThumb', 'strGlass', 'strIBA', 'strIngredient', 'strMeasure', 'strInstructions', 'strVideo']
+            column_names = ['Type (Alcoholic/Non Alcoholic)', 'Catégorie', 'Lien Image Verre', 'Type de verre', 'strIBA', 'Ingrédients', 'Quantités', 'Préparation', 'Vidéo']
+        # Beers
+        elif Db.choix_de_la_data_base == 2: 
+            real_names = ['brewery_name', 'beer_style', 'review_aroma', 'review_appearance', 'review_palate', 'review_taste', 'review_time','review_overall']
+            column_names = ['Brasseur', 'Style', 'Arômes', 'Apparence', 'Palais', 'Goût', 'Nombre d''évaluations', 'Review globale']
+        # Coffee
+        elif Db.choix_de_la_data_base == 3: 
+            real_names = ['loc_country', 'roaster', 'roast', '100g_USD', 'origin_', 'desc_', 'rating']
+            column_names = ['Provenance', 'Torréfacteur', 'Torréfaction', 'Prix', 'Origine', 'Description', 'Note (/100)']
+        # Mocktails
+        elif Db.choix_de_la_data_base == 4:
+            real_names = ['Ingredient ', 'Flavor Profile ', 'User Rating']
+            column_names = ['Ingrédients', 'Saveurs', 'Note (/5)']
+
+        #Création de la liste d'index pour récupérer les données
+        for name in real_names:
+                index = db_utilisee.columns.get_loc(name)
+                column_index.append(index)
+
+        #Récupération des données et mise en forme
+        for i in range(len(column_index)):
+            index = column_index[i]
+            column_name = column_names[i]
+            value = boisson[index]
+
+            # Convertir la valeur en chaîne de caractères
+            if isinstance(value, list):
+                value_str = ', '.join(str(item) for item in value)
+            elif column_name == 'Prix' and str(value) != 'nan':
+                if Db.choix_de_la_data_base == 0 : 
+                    value_str = f"{value} €"
+                elif Db.choix_de_la_data_base == 3: 
+                    value_str = f"{value} USD/100g"
+            else:
+                value_str = str(value)
+
+            if value_str.startswith('[') and value_str.endswith(']'):
+                value_list = [item.strip("' ") for item in value_str[1:-1].split(',')]
+                seen_elements = set()
+                value_list_filtered = [item for item in value_list if item != 'nan' and item != '' and item != ' ' and (item not in seen_elements and seen_elements.add(item) is None)]
+                value_str = ', '.join(value_list_filtered)
+            
+            if Db.choix_de_la_data_base == 2 and column_name == 'Arômes':
+                formatted_lines.append(f"Evaluations (/5) : ")
+                remaining_elements = [col for col in column_names[i+1:]]
+                for element in remaining_elements:
+                    if element == 'Nombre d''évaluations':
+                        formatted_lines.append(f"\n   - {element} : {int(boisson[column_index[column_names.index(element)]]):,}")
+                    else :
+                        formatted_lines.append(f"\n   - {element} : {boisson[column_index[column_names.index(element)]]:,}")
+            elif column_name not in ['Apparence', 'Palais', 'Goût', 'Nombre d''évaluations', 'Review globale'] and value_str != 'nan'and value_str != '' and value_str != ' ':
+                formatted_line = f"{column_name} : {value_str}"
+                formatted_lines.append(formatted_line)
+                formatted_lines.append('')
+
+        return '\n'.join(formatted_lines)  '''  
+    
+
+
+
+#Je le garde dns un coin mais jsp si on le garde : 
--- codes/UI/Screens/Description_page.py
+++ codes/UI/Screens/Description_page.py
@@ -14,6 +14,7 @@ import Research_page_UI as RU
 sys.path.append('codes/BackEnd/')
 import Db_gestions as Db
 import pandas as pd
+import Description_page_back as Dp
 
 class LabelPrincipal(QWidget):
     def __init__(self):
@@ -29,21 +30,14 @@ class LabelPrincipal(QWidget):
         layout.addWidget(self.drink_name)
 
     def update(self):
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        boisson = Db.dbsall[db][0].iloc[index]
-        db_utilisee = Db.dbsall[db][0]
-        index_name = db_utilisee.columns.get_loc('Name')
-        if str(boisson[index_name]) != 'nan' and str(boisson[index_name]) != '':
-            drink_name = boisson[index_name]
-        else :
-            drink_name = 'Nom non renseigné'
+        drink_name = Dp.get_name_from_drink()
         self.drink_name.setText(f'<font color="red"><b>Boisson : {drink_name}</b></font>')
 
 
 class InformationsDisplay(QScrollArea):
     def __init__(self):
         super().__init__()
+        self.setFixedSize(1000, 350)
 
         self.scroll_content = QWidget()
         layout = QVBoxLayout(self.scroll_content)
@@ -54,91 +48,18 @@ class InformationsDisplay(QScrollArea):
         self.description_text.setTextInteractionFlags(Qt.TextSelectableByMouse)
 
         layout.addWidget(self.description_text)
-
         self.setWidgetResizable(True)
         self.setWidget(self.scroll_content)
 
-        self.setFixedSize(1000, 350)
+        
 
     def update(self):
-        db =    Db.choix_de_la_data_base
-        index = Db.index_boisson
-        db_utilisee = Db.dbsall[db][0]
-        boisson = db_utilisee.iloc[index]
+        texte =  Dp.get_description_from_drink()
+        self.description_text.setText(texte)
 
-        formatted_text = self.format_text(boisson, db_utilisee)
-        self.description_text.setText(formatted_text)
-
-    # Mettre dans le backend cette fonction
-    def format_text(self, boisson, db_utilisee):
-        formatted_lines = []
-        column_index = []
         
-        # Wines
-        if Db.choix_de_la_data_base == 0:
-            real_names = ['country', 'description', 'price', 'province', 'variety', 'winery', 'region_']
-            column_names = ['Provenance', 'Description', 'Prix', 'Province', 'Variété', 'Domaine', 'Région']
-        # Cocktails
-        elif Db.choix_de_la_data_base == 1: 
-            real_names = ['strAlcoholic', 'strCategory', 'strDrinkThumb', 'strGlass', 'strIBA', 'strIngredient', 'strMeasure', 'strInstructions', 'strVideo']
-            column_names = ['Type (Alcoholic/Non Alcoholic)', 'Catégorie', 'Lien Image Verre', 'Type de verre', 'strIBA', 'Ingrédients', 'Quantités', 'Préparation', 'Vidéo']
-        # Beers
-        elif Db.choix_de_la_data_base == 2: 
-            real_names = ['brewery_name', 'beer_style', 'review_aroma', 'review_appearance', 'review_palate', 'review_taste', 'review_time','review_overall']
-            column_names = ['Brasseur', 'Style', 'Arômes', 'Apparence', 'Palais', 'Goût', 'Nombre d''évaluations', 'Review globale']
-        # Coffee
-        elif Db.choix_de_la_data_base == 3: 
-            real_names = ['loc_country', 'roaster', 'roast', '100g_USD', 'origin_', 'desc_', 'rating']
-            column_names = ['Provenance', 'Torréfacteur', 'Torréfaction', 'Prix', 'Origine', 'Description', 'Note (/100)']
-        # Mocktails
-        elif Db.choix_de_la_data_base == 4:
-            real_names = ['Ingredient ', 'Flavor Profile ', 'User Rating']
-            column_names = ['Ingrédients', 'Saveurs', 'Note (/5)']
 
-        #Création de la liste d'index pour récupérer les données
-        for name in real_names:
-                index = db_utilisee.columns.get_loc(name)
-                column_index.append(index)
 
-        #Récupération des données et mise en forme
-        for i in range(len(column_index)):
-            index = column_index[i]
-            column_name = column_names[i]
-            value = boisson[index]
-
-            # Convertir la valeur en chaîne de caractères
-            if isinstance(value, list):
-                value_str = ', '.join(str(item) for item in value)
-            elif column_name == 'Prix' and str(value) != 'nan':
-                if Db.choix_de_la_data_base == 0 : 
-                    value_str = f"{value} €"
-                elif Db.choix_de_la_data_base == 3: 
-                    value_str = f"{value} USD/100g"
-            else:
-                value_str = str(value)
-
-            if value_str.startswith('[') and value_str.endswith(']'):
-                value_list = [item.strip("' ") for item in value_str[1:-1].split(',')]
-                seen_elements = set()
-                value_list_filtered = [item for item in value_list if item != 'nan' and item != '' and item != ' ' and (item not in seen_elements and seen_elements.add(item) is None)]
-                value_str = ', '.join(value_list_filtered)
-            
-            if Db.choix_de_la_data_base == 2 and column_name == 'Arômes':
-                formatted_lines.append(f"Evaluations (/5) : ")
-                remaining_elements = [col for col in column_names[i+1:]]
-                for element in remaining_elements:
-                    if element == 'Nombre d''évaluations':
-                        formatted_lines.append(f"\n   - {element} : {int(boisson[column_index[column_names.index(element)]]):,}")
-                    else :
-                        formatted_lines.append(f"\n   - {element} : {boisson[column_index[column_names.index(element)]]:,}")
-            elif column_name not in ['Apparence', 'Palais', 'Goût', 'Nombre d''évaluations', 'Review globale'] and value_str != 'nan'and value_str != '' and value_str != ' ':
-                formatted_line = f"{column_name} : {value_str}"
-                formatted_lines.append(formatted_line)
-                formatted_lines.append('')
-
-        return '\n'.join(formatted_lines)
-
-##BENE  
 class FavoriteInteraction(QPushButton):
     def __init__(self):
         super().__init__()
@@ -153,25 +74,15 @@ class FavoriteInteraction(QPushButton):
         self.setFixedSize(1000,40)
 
     def update_icon(self):
-        
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        favory = Db.dbsall[db][0].iloc[index][-1]
-
-        if favory:
-            self.setIcon(self.star_icon_filled)
-        else:
-            self.setIcon(self.star_icon_empty)
+        if Dp.get_status_favori(): self.setIcon(self.star_icon_filled)
+        else: self.setIcon(self.star_icon_empty)
 
     def update_status(self):
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        favory = not(Db.dbsall[db][0].iloc[index][-1])
-        Db.dbsall[db][0].iloc[index,-1] = favory
+        Dp.update_status_favori()
         self.update_icon()
+        
 
 
-##BENE
 class CommentInteracton(QHBoxLayout):
     def __init__(self):
         super().__init__()
@@ -190,69 +101,48 @@ class CommentInteracton(QHBoxLayout):
 
 
     def update(self):
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        commentaire = Db.dbsall[db][0].iloc[index][-2]
         self.texte.setText('')
-        if   pd.isna(commentaire):
-            self.texte.setPlaceholderText('Laisser un commentaire sur votre boisson')
-        else :
-            self.texte.setPlaceholderText(commentaire)
+        commentaire =  Dp.get_comment()
+        self.texte.setPlaceholderText(commentaire)
     
     def comment(self):
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        Db.dbsall[db][0].iloc[index,-2] = self.texte.text()
+        Dp.update_comment(self.texte.text())
 
 
-class RatingInteraction(QWidget):
+
+class RatingInteraction(QHBoxLayout):
     def __init__(self):
         super().__init__()
-        rate_button = QPushButton(' Noter')
-        rate_button.setIcon(QIcon("codes/UI/Icones/rate.png"))
-        rate_button.setFixedSize(80,40)
-        rate_button.setStyleSheet("background-color: #404040; color: #ffffff;")
-        rate_button.clicked.connect(self.update_status)
 
-        stars_layout = QHBoxLayout()
-
+        self.prec_rating = 0
+        self.new_rating = 0 
         self.stars_images = []
         for i in range(1, 6):
             empty_star = QPixmap("codes/UI/Icones/star_empty.png").scaled(40, 40)
             filled_star = QPixmap("codes/UI/Icones/star_filled.png").scaled(40, 40)
             self.stars_images.append((empty_star, filled_star))
 
-        self.rating = 0
-
         self.star_buttons = []
         for i in range(5):
             star_button = QPushButton()
             star_button.setIcon(QIcon(self.stars_images[i][0]))
             star_button.setFixedSize(80, 40)
-            star_button.clicked.connect(lambda _, idx=i + 1: self.on_star_click(idx))
+            star_button.clicked.connect(lambda _, idx=i + 1: self.update_status(idx))
             star_button.installEventFilter(self)
             self.star_buttons.append(star_button)
 
             # Ajoute chaque étoile au layout horizontal
-            stars_layout.addWidget(star_button)
-
-        # Ajoute le bouton "Noter" et le layout des étoiles à ce widget
-        main_layout = QHBoxLayout()
-        main_layout.addStretch()
-        main_layout.addWidget(rate_button)
-        main_layout.addLayout(stars_layout)
-        main_layout.addStretch()
-        self.setLayout(main_layout)
+            self.addWidget(star_button)
 
     def on_star_click(self, index):
-        if index > self.rating:
+        if self.new_rating > self.prec_rating:
             for i in range(index):
                 self.star_buttons[i].setIcon(QIcon(self.stars_images[i][1]))
         else:
             for i in range(index, 5):
                 self.star_buttons[i].setIcon(QIcon(self.stars_images[i][0]))
         
-        self.rating = index
+        self.prec_rating = index
     
     def eventFilter(self, obj, event):
         if obj in self.star_buttons:
@@ -261,27 +151,24 @@ class RatingInteraction(QWidget):
                 for i in range(index):
                     self.star_buttons[i].setIcon(QIcon(self.stars_images[i][1]))
             elif event.type() == event.Leave:
-                if self.rating == 0:
+                if self.prec_rating == 0:
                     for i in range(5):
                         self.star_buttons[i].setIcon(QIcon(self.stars_images[i][0]))
                 else:
-                    for i in range(self.rating, 5):
+                    for i in range(self.prec_rating, 5):
                         self.star_buttons[i].setIcon(QIcon(self.stars_images[i][0]))
         return super().eventFilter(obj, event)
     
     def update_icon(self):
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        rating = Db.dbsall[db][0].iloc[index][-3]
+        rating = Dp.get_rating()
         self.on_star_click(int(rating))
 
-    def update_status(self):
-        db =  Db.choix_de_la_data_base
-        index = Db.index_boisson
-        Db.dbsall[db][0].iloc[index,-3] = self.rating
+    def update_status(self,idx):
+        self.new_rating = idx
+        Dp.update_rating(self.new_rating)
         self.update_icon()
-
-##BENE
+    
+    
 class NotationsInteractions(QVBoxLayout):
     def __init__(self, ):
         super().__init__()
@@ -292,7 +179,7 @@ class NotationsInteractions(QVBoxLayout):
 
 
         self.addWidget(self.favorite_interaction)
-        self.addWidget(self.rating_interaction)
+        self.addLayout(self.rating_interaction)
         self.addLayout(self.comment_interaction)
 
     def update(self):
